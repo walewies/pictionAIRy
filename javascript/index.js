d77ecbffefcd273436f44da39b6239f6cb1b46ae
Wian Lewies <lewieswian@gmail.com>
2024-07-07 20:38:17 +0200

    feat: adds support for iphone

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,35 @@
 function handleMotionEvent(event) {
-    const x = event.accelerationIncludingGravity.x;
-    const y = event.accelerationIncludingGravity.y;
-    const z = event.accelerationIncludingGravity.z;
-  
+    const x = event.acceleration.x;
+    const y = event.acceleration.y;
+    const z = event.acceleration.z;
+    
     // Do something awesome.
-    console.log(x);
-    console.log(y);
-    comsole.log(z);
-  }
-  
+    document.getElementById("accX").textContent = x;
+    document.getElementById("accY").textContent = y;
+    document.getElementById("accZ").textContent = z;       
+}
+
+if ( location.protocol != "https:" ) {
+    location.href = "https:" + window.location.href.substring( window.location.protocol.length );
+}
+
+function permission () {
+    if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
+        // (optional) Do something before API request prompt.
+        DeviceMotionEvent.requestPermission()
+            .then( response => {
+            // (optional) Do something after API prompt dismissed.
+            if ( response == "granted" ) {
+                window.addEventListener("devicemotion", handleMotionEvent)
+            }
+        })
+            .catch( console.error )
+    } else {
+        alert( "DeviceMotionEvent is not defined for this device" );
+    }
+}
+
+let btn = document.getElementById("request");
+btn.addEventListener("click", permission);
+
 window.addEventListener("devicemotion", handleMotionEvent, true);
-  
