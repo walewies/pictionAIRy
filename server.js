7e351ab381c6e4335a7034f29d303b0f92328da2
walewies <lewieswian@gmail.com>
2026-06-30 19:58:03 +0200

    chore: changes how routes are served from .html to file routes

--- server.js
+++ server.js
@@ -1,28 +1,55 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 8080;
-const IPADDRESS = "192.168.101.118";
 const https = require("https");
 const fs = require("fs");
+const { Server } = require("socket.io");
+
+const PORT = 8080;
+const IPADDRESS = "192.168.101.118";
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + "/html"));
-app.use(express.static(__dirname + "/javascript"));
-app.use(express.static(__dirname + "/css"));
+// Serve ONLY static assets
+app.use("/js", express.static(path.join(__dirname, "javascript")));
+app.use("/css", express.static(path.join(__dirname, "css")));
+app.use("/images", express.static(path.join(__dirname, "images"))); // if you have one
 
 const options = {
-    key: fs.readFileSync(__dirname + "/keys/server.key"),
-    cert: fs.readFileSync(__dirname + "/keys/server.cert"),
+    key: fs.readFileSync(path.join(__dirname, "keys", "server.key")),
+    cert: fs.readFileSync(path.join(__dirname, "keys", "server.cert")),
 };
 
+// HTML routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "html", "index.html"));
 });
 
-https.createServer(options, app).listen(PORT, () => {
+app.get("/join-room", (req, res) => {
+    res.sendFile(path.join(__dirname, "html", "join-room.html"));
+});
+
+app.get("/create-room", (req, res) => {
+    res.sendFile(path.join(__dirname, "html", "create-room.html"));
+});
+
+// Create HTTPS server
+const server = https.createServer(options, app);
+
+// Initialize Socket.IO
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+    socket.on("disconnect", () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    });
+});
+
+// Start server
+server.listen(PORT, () => {
     console.log(`Listening @ https://${IPADDRESS}:${PORT}`);
-})
+});
